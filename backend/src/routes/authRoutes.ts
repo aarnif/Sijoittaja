e178c3f853a178a42b5670ac0d1ab0a1ac5917cd
Ronja <ronja.sonninen@helsinki.fi>
2026-01-28 11:25:22 +0200

    fix: correct file and delete wrong one

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { mockAuthMiddleware } from "../middleware/mockAuth.middleware";
+import { mockAuthMiddleware } from "../middleware/mockAuth";
 
 const router = Router();
 
